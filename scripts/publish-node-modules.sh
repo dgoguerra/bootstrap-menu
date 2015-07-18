0d500f024fe6dde8a0ff2ae7fa871fe37a289d23
Diego Guerra <dgoguerra.or@gmail.com>
2015-07-18 22:17:52 +0200

    added postinstall script to update deps in javascripts/ and stylesheets/
    The script moves the dependencies needed for demo.html out of
    node_modules/, so that they are saved in the repo.
    
    This allows the pages to be served as static pages through GitHub,
    without libraries dependencies loaded from a CDN.

---
+++ scripts/publish-node-modules.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cp node_modules/bootstrap/dist/css/bootstrap.css \
+   node_modules/toastr/toastr.css \
+   stylesheets/
+
+
+cp node_modules/jquery/dist/jquery.js \
+   node_modules/lodash/index.js \
+   node_modules/bootstrap/dist/js/bootstrap.js \
+   node_modules/toastr/toastr.js \
+   node_modules/bootstrap-menu/ContextMenu.js \
+   javascripts/
+
