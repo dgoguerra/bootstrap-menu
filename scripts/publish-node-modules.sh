47c62228b1c347af7648427a3fd7a051a2b5b6cc
Diego Guerra <dgoguerra.or@gmail.com>
2015-07-23 15:22:16 +0200

    demos updated for bootstrap-menu@1.0.5

--- scripts/publish-node-modules.sh
+++ scripts/publish-node-modules.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 cp node_modules/bootstrap/dist/css/bootstrap.css \
+   node_modules/font-awesome/css/font-awesome.css \
    node_modules/toastr/toastr.css \
    stylesheets/
+
+cp node_modules/font-awesome/fonts/fontawesome-webfont.eot \
+   node_modules/font-awesome/fonts/fontawesome-webfont.svg \
+   node_modules/font-awesome/fonts/fontawesome-webfont.svg \
+   node_modules/font-awesome/fonts/fontawesome-webfont.woff2 \
+   fonts/
 
 
 cp node_modules/jquery/dist/jquery.js \
