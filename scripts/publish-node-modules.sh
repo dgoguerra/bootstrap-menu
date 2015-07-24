d956913c167ca7e1b9907ab68cc932e638641846
Diego Guerra <dgoguerra.or@gmail.com>
2015-07-25 00:59:07 +0200

    updated for bootstrap-menu@1.0.6

--- scripts/publish-node-modules.sh
+++ scripts/publish-node-modules.sh
@@ -1,21 +1,8 @@
 #!/usr/bin/env bash
 
-cp node_modules/bootstrap/dist/css/bootstrap.css \
-   node_modules/font-awesome/css/font-awesome.css \
-   node_modules/toastr/toastr.css \
+cp node_modules/toastr/toastr.css \
    stylesheets/
 
-cp node_modules/font-awesome/fonts/fontawesome-webfont.eot \
-   node_modules/font-awesome/fonts/fontawesome-webfont.svg \
-   node_modules/font-awesome/fonts/fontawesome-webfont.svg \
-   node_modules/font-awesome/fonts/fontawesome-webfont.woff2 \
-   fonts/
-
-
-cp node_modules/jquery/dist/jquery.js \
-   node_modules/lodash/index.js \
-   node_modules/bootstrap/dist/js/bootstrap.js \
-   node_modules/toastr/toastr.js \
-   node_modules/bootstrap-menu/ContextMenu.js \
+cp node_modules/toastr/toastr.js \
+   node_modules/bootstrap-menu/dist/BootstrapMenu.min.js \
    javascripts/
-
